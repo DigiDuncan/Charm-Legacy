932fdba7eaf556a65ab29941811364bef815c481
DigiDuncan <digiduncan@gmail.com>
2021-02-05 03:04:53 -0500

    just, you know what? no lasergame.

--- charm/lib/assets/__init__.py
+++
@@ -1,9 +0,0 @@
-from .fontmanager import FontManager
-from .imagemanager import ImageManager
-from .soundmanager import SoundManager
-
-__all__ = ["fonts", "images", "sounds"]
-
-fonts = FontManager()
-images = ImageManager()
-sounds = SoundManager()
