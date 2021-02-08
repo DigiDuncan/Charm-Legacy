cc2ef154faae284647978a81e4d3c2d75903e625
DigiDuncan <digiduncan@gmail.com>
2021-02-07 19:05:01 -0500

    move test into main

--- charm/main.py
+++ charm/main.py
@@ -1,9 +1,17 @@
-# import pygame
+import pygame
 # from charm.lib import nygame
 
 
+def setup():
+    pygame.init()
+
+    flags = pygame.DOUBLEBUF
+
+    pygame.display.set_mode((800, 600), flags)
+
+
 def main():
-    print("hey")
+    setup()
 
 
 # This is needed, or else calling `python -m <name>` will mean that main() is called twice.
