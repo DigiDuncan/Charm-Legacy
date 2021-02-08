a660342a8d7e2618cf2f3720416aee5efd141a46
DigiDuncan <digiduncan@gmail.com>
2021-02-07 20:04:22 -0500

    add instrument names

--- charm/lib/constants.py
+++ charm/lib/constants.py
@@ -6,21 +6,26 @@ import charm.data
 from charm.lib.attrdict import AttrDict
 
 frets = None
+instruments = None
 
 
 def loadfrets(data):
     global frets
     fretsdict = data.get("frets", {})
-    # make all names lowercase
-    fretsdict = {name.lower(): value for name, value in fretsdict.items()}
-    # create the enum
     frets = AttrDict(fretsdict)
+
+
+def loadinstruments(data):
+    global instruments
+    instrumentsdict = data.get("instruments", {})
+    instruments = AttrDict(instrumentsdict)
 
 
 def load():
     # Load constants toml file
     data = toml.loads(pkg_resources.read_text(charm.data, "constants.toml"))
     loadfrets(data)
+    loadinstruments(data)
 
 
 load()
