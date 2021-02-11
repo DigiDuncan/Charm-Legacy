920257758c9c73b8589d2bd2245667a1e57f016a
DigiDuncan <digiduncan@gmail.com>
2021-02-10 23:20:10 -0500

    better song data?

--- charm/classes/songdata.py
+++ charm/classes/songdata.py
@@ -1,118 +1,166 @@
-from dataclasses import dataclass, field
-from typing import List, Literal
+from __future__ import annotations
 
-from charm.lib.njson import jsonable
-
-
-@jsonable
-@dataclass
-class Note:
-    """A single note.
-    `fret: int`, 0 = open, 1-5 = green, red, yellow, blue, orange, (or 1-6 for GHL, etc)
-    `length: int` the length in ticks? of this note
-    """
-    fret: int
-    length: int
+from typing import Dict, List, Tuple
 
 
-@jsonable
-@dataclass
-class Chord:
-    """A chord (many notes together).
-    `position: int`, this chords position in the chart.
-    `flag: str`, either normal, hopo, or tap (can't be hopo and tap)
-    `star_power: bool`, this note is part of a star power phrase
-    `notes: List[Note]`, a list of notes.
-    """
-    position: int
-    flag: Literal["normal", "hopo", "tap", "forced"]
-    notes: List[Note]
+class Timestamp:
+    def __init__(self, ticks: int):
+        self.ticks = ticks
+        self.time: float = None
 
-    @property
-    def longest_note_length(self):
-        ln = 0
-        for note in self.notes:
-            if note.length > ln:
-                ln - note.length
-        return ln
+    def ticks_to_time(self, song: "Song") -> float:
+        return self.ticks / 440  # TODO: Placeholder
+
+    def time_to_ticks(self, song: "Song") -> int:
+        return self.time * 440  # TODO: Placeholder
 
 
-@jsonable
-@dataclass
-class BPMEvent:
-    position: int
-    bpm: float
+class TimeDelta:
+    def __init__(self, position: Timestamp, ticks: int):
+        self.position = position
+        self.ticks = ticks
+        self.length: float = None
+    # TODO: This is not finished at all.
 
 
-@jsonable
-@dataclass
-class TSEvent:
-    position: int
-    time_sig: tuple
+class Event:
+    def __init__(self, song: "Song", position: Timestamp):
+        self.song = song
+        self.position = position
 
 
-@jsonable
-@dataclass
-class SPEvent:
-    position: int
-    length: int
+class ChartEvent(Event):
+    def __init__(self, song: "Song", chart: "Chart", position: Timestamp):
+        self.chart = chart
+        super().__init__(song, position)
 
 
-@jsonable
-@dataclass
+class Lyric(Event):
+    def __init__(self, song: "Song", position: Timestamp,
+                 word: str, partial: bool, length: TimeDelta):
+        self.word = word
+        self.partial = partial
+        self.length = length
+        super().__init__(song, position)
+
+
+class LyricPhrase(Event):
+    def __init__(self, song: "Song", position: Timestamp,
+                 length: TimeDelta, lyrics: List[Lyric]):
+        self.length = length
+        self.lyrics = lyrics
+        super().__init__(song, position)
+
+
+class BPMEvent(Event):
+    def __init__(self, song: "Song", position: Timestamp,
+                 bpm: float):
+        self.bpm = bpm
+        super().__init__(song, position)
+
+
+class TSEvent(Event):
+    def __init__(self, song: "Song", position: Timestamp,
+                 time_signature: Tuple[int, int]):
+        self.time_signature = time_signature
+        super().__init__(song, position)
+
+
+class GenericEvent(Event):
+    def __init__(self, song: "Song", position: Timestamp,
+                 data: dict):
+        self.data = data
+        super().__init__(song, position)
+
+
+class Note(ChartEvent):
+    def __init__(self, song: "Song", chart: "Chart", position: Timestamp,
+                 fret: int, flag: str, length: TimeDelta):
+        self.fret = fret
+        self.flag = flag  # Flags might need to be bytes? ¯\_(ツ)_/¯
+        self.length = length
+        super().__init__(song, chart, position)
+
+
+class Chord(ChartEvent):
+    def __init__(self, song: "Song", chart: "Chart", position: Timestamp,
+                 notes: List[Note]):
+        self.notes = notes
+        self.flag = self.notes[0].flag  # This might not work.
+        super().__init__(song, chart, position)
+
+
+class SPEvent(ChartEvent):
+    def __init__(self, song: "Song", chart: "Chart", position: Timestamp,
+                 length: TimeDelta):
+        self.length = length
+        super().__init__(song, chart, position)
+
+
+class GenericChartEvent(ChartEvent):
+    def __init__(self, song: "Song", chart: "Chart", position: Timestamp,
+                 data: dict):
+        self.data = data
+        super().__init__(song, chart, position)
+
+
 class Chart:
-    instrument: str
-    difficulty: int
-    chords: List[Chord]
-    bpm_events: List[BPMEvent]
-    star_power_events: List[SPEvent] = field(default_factory=list)
-    time_sig_events: List[TSEvent] = TSEvent(0, (4, 4))
+    def __init__(self, song: "Song", instrument: str, difficulty: int, events: List[ChartEvent]):
+        self.song = song
+        self.instrument = instrument
+        self.difficulty = difficulty
+        self.events = events
 
     @property
-    def last_chord(self) -> Chord:
-        lc = self.chords[0]
-        for chord in self.chords:
-            if chord.position > lc.position:
-                lc = chord
-        return lc
+    def notes(self):
+        return [e for e in self.events if isinstance(e, Note)]
+
+    @property
+    def chords(self):
+        last_note = None
+        current_chord = []
+        _chords = []
+        for note in self.notes:
+            if last_note is None or last_note.position == note.position:
+                current_chord.append(note)
+            else:
+                _chords.append(Chord(self.song, self, current_chord[0].position, current_chord))
+            last_note = note
+        _chords.append(Chord(self.song, self, current_chord[0].position, current_chord))
+        return _chords
+
+    def get_notes(self, start: Timestamp, stop: Timestamp) -> List[Note]:
+        return [n for n in self.notes if n >= start and n <= stop]
+
+    def get_chords(self, start: Timestamp, stop: Timestamp) -> List[Chord]:
+        return [c for c in self.chords if c >= start and c <= stop]
+
+    def get_events(self, start: Timestamp, stop: Timestamp) -> List[Event]:
+        return [e for e in self.events if e >= start and e <= stop]
 
 
-@jsonable
-@dataclass
-class Lyric:
-    word: str
-    rel_position: int
-    length: int
-    append_space: bool = True
-
-
-@jsonable
-@dataclass
-class LyricPhrase:
-    position: int
-    length: int
-    lyrics: List[Lyric]
-
-
-@jsonable
-@dataclass
 class Song:
-    name: str
-    artist: str
-    album: str
-    difficulty: int
-    charter: str
-    year: int
-    genre: str
-    track: int
-    length: int
-    resolution: int
-    offset: int
-    charts: List[Chart]
-    lyricphrases: List[LyricPhrase]
+    def __init__(self, name: str, artist: str, album: str, track: int, year: str, genre: str, rating: int, charter: str, length: int,
+                 resolution: int, offset: int, charts: Dict[Tuple[str, int], Chart] = None, lyricphrases: List[LyricPhrase] = None,
+                 alt_name: str = None):
+        self.name = name
+        self.alt_name = alt_name  # Some songs in some games (DDR, etc.) have an alt-name, usually a Romanji version.
+        self.artist = artist
+        self.album = album
+        self.track = track
+        self.year = year
+        self.genre = genre
+        self.rating = rating
+        self.charter = charter
+        self.length = length  # Do not use for anything but display/sorting.
 
-    def get_chart(self, instrument, difficulty):
-        for c in self.charts:
-            if c.instrument == instrument and c.difficulty == difficulty:
-                return c
-        return None
+        self.resolution = resolution
+        self.offset = offset
+        self.charts = charts
+        self.lyricphrases = lyricphrases
+
+    def get_chart(self, instrument: str, difficulty: int) -> Chart:
+        return self.charts[(instrument, difficulty)]
+
+    def set_chart(self, instrument: str, difficulty: int, chart: Chart):
+        self.charts[(instrument, difficulty)] = chart
