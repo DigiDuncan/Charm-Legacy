59d0fb460be9260503a9ba2de6355acf6fd64190
DigiDuncan <digiduncan@gmail.com>
2021-02-14 16:21:31 -0500

    it's a real test wow

--- charm/test/chparse_test.py
+++ charm/test/chparse_test.py
@@ -3,10 +3,19 @@ from pathlib import Path
 import chparse
 import tqdm
 
-charts = Path("F:/chs").rglob("notes.*")
+in_path = input("Recursively search what directory for charts? > ")
+in_limit = input("Limit number of charts scanned to... (hit ENTER for no limit) > ")
+if in_limit:
+    in_number = in_limit
+else:
+    in_number = input("How many charts are you expecting? > ")
+
+in_limit = int(in_limit)
+in_number = int(in_number)
+
+charts = Path(in_path).rglob("notes.*")  # Clone Hero requires charts be named "notes.chart" or "notes.mid[i]."
 
 bad_charts = {}
-limit = None
 
 i = 0
 
@@ -14,11 +23,12 @@ chart_c = 0
 mid_c = 0
 other_c = 0
 
-for chart in tqdm.tqdm(charts, unit = " charts", total = 18600):
+print("Processing charts...")
+for chart in tqdm.tqdm(charts, unit = " charts", total = in_number):
     i += 1
-    if limit is not None and i > limit:
+    if in_limit is not None and i > in_limit:
         break
-    if chart.suffix == ".mid" or chart.suffix == ".midi":
+    if chart.suffix.lower() in [".mid", ".midi"]:  # midis are so old sometimes the extension is uppercase
         mid_c += 1
     elif chart.suffix == ".chart":
         chart_c += 1
@@ -29,18 +39,30 @@ for chart in tqdm.tqdm(charts, unit = " charts", total = 18600):
                 raise ValueError("Don't be None!")
         except Exception as e:
             bad_charts[str(chart.absolute())] = e
-    else:
+    else:  # .bak, .backup, .eof
         other_c += 1
 
 errors = {}
 raw_errors = {}
 
-for c, e in tqdm.tqdm(bad_charts.items()):
+print("\nProcessing errors...")
+for c, e in tqdm.tqdm(bad_charts.items(), unit = " errors"):
     raw_errors[c] = str(e)
     if str(e) in errors.keys():
         errors[str(e)] += 1
     else:
         errors[str(e)] = 1
-print(raw_errors)
-print(errors)
-print(f".charts: {chart_c}\n.mids: {mid_c}\nOthers: {other_c}\nTotal Charts: {chart_c + mid_c}\nTotal: {chart_c + mid_c + other_c}")
+
+# Print the errors
+print("\n=== RAW ERRORS ===")
+for name, error in raw_errors.items():
+    print(f"{name}: {error}")
+if raw_errors == {}:
+    print("No errors!")
+print("\n=== ERROR COUNT ===")
+for e in sorted(errors, key=errors.get, reverse=True):
+    print(f"{e}: {errors[e]}")
+if errors == {}:
+    print("No errors!")
+print("\n=== TOTALS ===")
+print(f".charts: {chart_c}\n.mids: {mid_c}\nOthers: {other_c}\nTotal Charts: {chart_c + mid_c}\nTotal: {chart_c + mid_c + other_c}\nErrors: {len(raw_errors)}")
