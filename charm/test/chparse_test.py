6fc012c0b33a61c5032026691b518e000bb95252
DigiDuncan <digiduncan@gmail.com>
2021-02-14 04:29:45 -0500

    the best test

--- charm/test/chparse_test.py
+++ charm/test/chparse_test.py
@@ -1,30 +1,46 @@
-import importlib.resources as pkg_resources
+from pathlib import Path
 
 import chparse
-from chparse import Difficulties, Instruments
+import tqdm
 
-from charm.data import example
+charts = Path("F:/chs").rglob("notes.*")
 
-with pkg_resources.open_text(example, "notes_moon.chart", "utf-8 sig") as f:
-    chart = chparse.parse.load(f)
+bad_charts = {}
+limit = None
 
+i = 0
 
-def test():
-    print(repr(chart))
-    sections = {}
-    notes = []
-    lyrics = {}
-    sync = {}
-    for event in chart.events:
-        if event.event.startswith("section"):
-            sections[event.time] = event.event.removeprefix("section ")
-        elif event.event.startswith("lyric"):
-            lyrics[event.time] = event.event.removeprefix("lyric ")
-    for note in chart.instruments[Difficulties.EXPERT][Instruments.GUITAR]:
-        notes.append(note)
+chart_c = 0
+mid_c = 0
+other_c = 0
 
-    print(
-        f"Sections: {sections}\n"
-        f"Lyrics: {lyrics}\n"
-        f"Notes {notes}"
-    )
+for chart in tqdm.tqdm(charts, unit = " charts", total = 18600):
+    i += 1
+    if limit is not None and i > limit:
+        break
+    if chart.suffix == ".mid" or chart.suffix == ".midi":
+        mid_c += 1
+    elif chart.suffix == ".chart":
+        chart_c += 1
+        try:
+            with chart.open(encoding = "utf-8 sig") as f:
+                c = chparse.parse.load(f)
+            if c is None:
+                raise ValueError("Don't be None!")
+        except Exception as e:
+            bad_charts[str(chart.absolute())] = e
+    else:
+        other_c += 1
+
+errors = {}
+raw_errors = {}
+
+for c, e in tqdm.tqdm(bad_charts.items()):
+    raw_errors[c] = str(e)
+    if str(e) in errors.keys():
+        errors[str(e)] += 1
+    else:
+        errors[str(e)] = 1
+print(raw_errors)
+print(errors)
+print(f".charts: {chart_c}\n.mids: {mid_c}\nOthers: {other_c}\nTotal Charts: {chart_c + mid_c}\nTotal: {chart_c + mid_c + other_c}")
