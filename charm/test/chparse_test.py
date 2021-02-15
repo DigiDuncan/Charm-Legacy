ae1566690f78d978a43d23ae8a52a65ea4742a65
DigiDuncan <digiduncan@gmail.com>
2021-02-14 20:28:47 -0500

    the bestest testest.

--- charm/test/chparse_test.py
+++ charm/test/chparse_test.py
@@ -1,21 +1,44 @@
+import re
 from pathlib import Path
+from traceback import format_exc
+from zipfile import ZipFile, ZIP_DEFLATED
 
 import chparse
 import tqdm
 
+
+def zip_all(dst, *, copy=None, create=None):
+    if copy is None:
+        copy = {}
+    if create is None:
+        create = {}
+    with ZipFile(dst, mode="w", compression=ZIP_DEFLATED) as z:
+        print("Copying files to zip...")
+        for newname, srcpath in tqdm.tqdm(copy.items(), unit = " files"):
+            data = Path(srcpath).read_bytes()
+            z.writestr(newname, data)
+        print("Writing logs to zip...")
+        for newname, data in tqdm.tqdm(create.items(), unit = " files"):
+            z.writestr(newname, data)
+
+
 in_path = input("Recursively search what directory for charts? > ")
 in_limit = input("Limit number of charts scanned to... (hit ENTER for no limit) > ")
-if in_limit:
+if in_limit != "":
     in_number = in_limit
+    in_limit = int(in_limit)
 else:
-    in_number = input("How many charts are you expecting? > ")
-
-in_limit = int(in_limit)
-in_number = int(in_number)
+    in_number = input("How many charts are you expecting? (hit ENTER for unknown) > ")
+    in_limit = None
+    if in_number == "":
+        in_number = None
+    else:
+        in_number = int(in_number)
 
 charts = Path(in_path).rglob("notes.*")  # Clone Hero requires charts be named "notes.chart" or "notes.mid[i]."
 
 bad_charts = {}
+bad_charts_expanded = {}
 
 i = 0
 
@@ -39,36 +62,49 @@ for chart in tqdm.tqdm(charts, unit = " charts", total = in_number):
                 raise ValueError("Don't be None!")
         except Exception as e:
             bad_charts[str(chart.absolute())] = e
+            bad_charts_expanded[str(chart.absolute())] = format_exc()
     else:  # .bak, .backup, .eof
         other_c += 1
 
 errors = {}
 raw_errors = {}
+sources = {}
+full_errors = {}
 
 print("\nProcessing errors...")
 for c, e in tqdm.tqdm(bad_charts.items(), unit = " errors"):
-    raw_errors[c] = str(e)
+    test_path = in_path.replace("\\", "/")
+    full_path = c.replace("\\", "/")
+    new_path = re.search(f"{test_path}/(.*)/notes", full_path).group(1).replace("/", " - ")
+    sources[new_path + ".chart"] = c
+    full_errors[new_path + ".error"] = bad_charts_expanded[c]
+
+    raw_errors[new_path] = str(e)
     if str(e) in errors.keys():
         errors[str(e)] += 1
     else:
         errors[str(e)] = 1
 
-# Print the errors
-print("\n=== RAW ERRORS ===")
+print("\nGenerating output file...")
+
+out = ""
+out += ("=== RAW ERRORS ===\n")
 for name, error in raw_errors.items():
-    print(f"{name}: {error}")
+    out += (f"\"{name}\": {error}\n")
 if raw_errors == {}:
-    print("No errors!")
-print("\n=== ERROR COUNT ===")
+    out += ("No errors!\n")
+out += ("\n\n=== ERROR COUNT ===\n")
 for e in sorted(errors, key=errors.get, reverse=True):
-    print(f"{e}: {errors[e]}")
+    out += (f"{e}: {errors[e]}\n")
 if errors == {}:
-    print("No errors!")
-print("\n=== TOTALS ===")
-print(f".charts: {chart_c}\n"
-      f".mids: {mid_c}\n"
-      f"Others: {other_c}\n"
-      f"Total Charts: {chart_c + mid_c}\n"
-      f"Total: {chart_c + mid_c + other_c}\n"
-      f"Errors: {len(raw_errors)}"
-      f"Success Rate: {round((chart_c + mid_c - len(raw_errors)) / (chart_c + mid_c) * 100, 3)}%")
+    out += ("No errors!\n")
+out += ("\n\n=== TOTALS ===\n")
+out += (f".charts: {chart_c}\n"
+        f".mids: {mid_c}\n"
+        f"Others: {other_c}\n"
+        f"Total Charts: {chart_c + mid_c}\n"
+        f"Total: {chart_c + mid_c + other_c}\n"
+        f"Errors: {len(raw_errors)}\n"
+        f"Success Rate: {round((chart_c + mid_c - len(raw_errors)) / (chart_c + mid_c) * 100, 3)}%")
+
+zip_all("output.zip", copy = sources, create = full_errors | {"log.txt": out})
