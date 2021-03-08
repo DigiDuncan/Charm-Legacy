f6f0d72ce58c6729524233e01340ee42fa6c68f5
Natalie Fearnley <nfearnley@gmail.com>
2021-03-08 04:49:14 -0500

    Added very basic loading of lyrics from chart files

--- charm/test/lyric_animation_test.py
+++ charm/test/lyric_animation_test.py
@@ -1,102 +1,33 @@
+from pathlib import Path
+from charm.test.lyrics.loader import load_lyrics
 from charm.lib.utils import cache_on
-from charm.test.lyrics.run_around import lyrics
 
 import pygame
 import pygame.freetype
 import pygame.draw
 
 from nygame import DigiText as T
-from nygame.emoji import emojize
 
 
-def clean_word(word: str):
-    if word.endswith("-"):
-        word = word.removesuffix("-")
-    else:
-        word = word + " "
-    word = word.replace("=", "-").replace("''", "\"").replace("+", "").replace("#", "").replace("^", "")
-    word = emojize(word)
-    return word
-
-
-class LyricWord():
-    def __init__(self, offset, text):
-        self.offset = offset
-        self.text = clean_word(text)
-
-    def is_on(self, curr_offset):
-        return self.offset <= curr_offset
-
-    def __lt__(self, other):
-        return self.offset < other.offset
-
-    @classmethod
-    def parse(cls, j):
-        return cls(j["time"], j["word"])
-
-
-class LyricPhrase():
-    def __init__(self, start, end, words):
-        self.start = start
-        self.end = end
-        self.words = sorted(LyricWord.parse(w) for w in words)
-        self.words[-1].text = self.words[-1].text.rstrip()
-        self.next = None
-
-    def get_on_text(self, tracktime):
-        return "".join(w.text for w in self.words if w.is_on(tracktime - self.start))
-
-    def get_off_text(self, tracktime):
-        return "".join(w.text for w in self.words if not w.is_on(tracktime - self.start))
-
-    def get_text(self):
-        return "".join(w.text for w in self.words)
-
-    def render_to(self, surf, tracktime, fontname, offset=(0, 0)):
-        full_text = self.get_text()
-        on_text = self.get_on_text(tracktime)
-        off_text = self.get_off_text(tracktime)
-        fontsize = fit_text(full_text, fontname, surf.get_width() * 0.75, 24)
-        T.font = fontname
-        T.size = fontsize
-        digitext = T(on_text, color="#ffff00") + T(off_text, color="#808080")
-        dest_rect = digitext.get_rect()
-        dest_rect.centerx = surf.get_rect().centerx
-        dest_rect.y = surf.get_rect().centery
-        dest_rect.move_ip(offset)
-        digitext.render_to(surf, dest_rect)
-
-    @property
-    def fade(self):
-        if self.next is None:
-            return 1
-        return min(self.next.start - self.end, 1)
-
-    @property
-    def fade_end(self):
-        return self.end + self.fade
-
-    def is_active(self, tracktime):
-        return self.start <= tracktime < self.end
-
-    def is_waiting(self, tracktime):
-        return self.start > tracktime
-
-    def is_done(self, tracktime):
-        return self.end <= tracktime
-
-    def __lt__(self, other):
-        return self.start < other.start
-
-    @classmethod
-    def parse(cls, j):
-        return cls(j["time"], j["end_time"], j["words"])
+def render_phrase_to(phrase, surf, tracktime, fontname, offset=(0, 0)):
+    full_text = phrase.get_text()
+    on_text = phrase.get_on_text(tracktime)
+    off_text = phrase.get_off_text(tracktime)
+    fontsize = fit_text(full_text, fontname, surf.get_width() * 0.75, 24)
+    T.font = fontname
+    T.size = fontsize
+    digitext = T(on_text, color="#ffff00") + T(off_text, color="#808080")
+    dest_rect = digitext.get_rect()
+    dest_rect.centerx = surf.get_rect().centerx
+    dest_rect.y = surf.get_rect().centery
+    dest_rect.move_ip(offset)
+    digitext.render_to(surf, dest_rect)
 
 
 class LyricAnimator:
-    def __init__(self, phrases: list = lyrics, *,
-                 size: tuple = (600, 500), font = "Segoe UI Emoji", show_next=False):
-        self.phrases = sorted(LyricPhrase.parse(p) for p in phrases)
+    def __init__(self, *, size: tuple = (600, 500), font = "Segoe UI Emoji", show_next=False):
+        with Path("./charm/test/lyrics/run_around_the_character_code.chart").open("r", encoding="utf-8") as f:
+            self.phrases = load_lyrics(f)
         self.width, self.height = size
         self.font = font
         self._image = None
@@ -152,10 +83,10 @@ class LyricAnimator:
         pygame.draw.line(surf, "red", surf.get_rect().midleft, surf.get_rect().midright)
 
         if self.active_phrase is not None:
-            self.active_phrase.render_to(surf, self.tracktime, self.font)
+            render_phrase_to(self.active_phrase, surf, self.tracktime, self.font)
 
         if self.show_next and self.next_phrase is not None:
-            self.next_phrase.render_to(surf, self.tracktime, self.font, offset=(0, 40))
+            render_phrase_to(self.next_phrase, surf, self.tracktime, self.font, offset=(0, 40))
 
         return surf
 
