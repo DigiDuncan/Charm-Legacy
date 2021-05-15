1b628df224f4ddbdea0fa08f6f28abc950f914a7
DigiDuncan <digiduncan@gmail.com>
2021-05-14 20:16:34 -0400

    show that the menu can do something

--- charm/prototyping/menu/menu.py
+++ charm/prototyping/menu/menu.py
@@ -19,7 +19,7 @@ def stacksurfs(surf1: Surface, surf2: Surface, gap = 0) -> Surface:
     height = surf1.get_height() + surf2.get_height() + gap
     yoffset = surf1.get_height() + gap
 
-    newsurf = Surface((width, height))
+    newsurf = pygame.Surface((width, height))
     newsurf.blit(surf1, (0, 0))
     newsurf.blit(surf2, (0, yoffset))
 
@@ -49,6 +49,10 @@ class Menu:
         self.items = items
         self.selected = 0
 
+    @property
+    def selected_item(self):
+        return self.items[self.selected]
+
     def sort(self, key, reverse = False):
         self.items = self.sorted(key = key, reverse = reverse)
 
@@ -75,12 +79,12 @@ class Game(nygame.Game):
     def __init__(self):
         super().__init__(fps = 120, showfps = True)
         self.data = [
-            {"title": "Never Gonna Give You Up", "artist": "Rick Astley"},
-            {"title": "Ace of Spades", "artist": "Motorhead"},
-            {"title": "Sonic Forces (Fist Bump)", "artist": "Crush 40"},
-            {"title": "Less Talk More Rokk", "artist": "Freezepop"}
+            {"key": "nevergonnagiveyouup", "title": "Never Gonna Give You Up", "artist": "Rick Astley"},
+            {"key": "aceofspades", "title": "Ace of Spades", "artist": "Motorhead"},
+            {"key": "fistbump", "title": "Sonic Forces (Fist Bump)", "artist": "Crush 40"},
+            {"key": "lesstalkmorerokk", "title": "Less Talk More Rokk", "artist": "Freezepop"}
         ]
-        self.items = [MenuItem(i["title"], i["title"], i["artist"]) for i in self.data]
+        self.items = [MenuItem(i["key"], i["title"], i["artist"]) for i in self.data]
         self.menu = Menu(self.items)
         self.selected = 0
         self.sorttypes = cycle(["title", "artist"])
@@ -101,8 +105,15 @@ class Game(nygame.Game):
         self.menu.selected = self.selected
 
         self.menu.update()
+
+        sel_item = T(self.menu.selected_item.key, font = "Lato Medium", size = 16, color = "yellow").render()
+        sel_dest = sel_item.get_rect()
+        sel_dest.bottomleft = self.surface.get_rect().bottomleft
+
         menusurf = self.menu.render()
+
         self.surface.blit(menusurf, (5, 5))
+        self.surface.blit(sel_item, sel_dest)
 
 
 Game().run()
