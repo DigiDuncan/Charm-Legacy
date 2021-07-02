55355679f07b017ea13988f95aa5bb8fa3e9e658
DigiDuncan <digiduncan@gmail.com>
2021-07-02 14:16:51 -0400

    typing for videoplayer

--- charm/prototyping/videoplayer/videoplayer.py
+++ charm/prototyping/videoplayer/videoplayer.py
@@ -1,26 +1,28 @@
 import cv2
 
 import arrow
+import numpy
 
 import nygame
 import pygame
 
 
-def cv2pg(frame_yx_bgr):
+def cv2pg(frame_yx_bgr: numpy.ndarray) -> numpy.ndarray:
     frame_yx_rgb = cv2.cvtColor(frame_yx_bgr, cv2.COLOR_BGR2RGB)
     frame_xy_rgb = frame_yx_rgb.swapaxes(0, 1)
     return frame_xy_rgb
 
 
 class VideoPlayer:
-    def __init__(self, videopath):
+    def __init__(self, videopath: str):
         self.cap = cv2.VideoCapture(videopath)
         self.frame = None
         self.start_time = arrow.now()
 
-    def get_frame(self, time):
+    def get_frame(self, time: float) -> numpy.ndarray:
         while self.cap.get(cv2.CAP_PROP_POS_MSEC) < time * 1000:
-            self.frame = cv2pg(self.cap.read()[1])
+            frame = self.cap.read()[1]
+            self.frame = cv2pg(frame)
         return self.frame
 
 
@@ -29,7 +31,7 @@ class Game(nygame.Game):
         super().__init__(size = (1280, 720), fps = 120, showfps = True, bgcolor="pink")
         self.videoplayer = VideoPlayer("C:/Users/digid/Videos/Awoken.mp4")
 
-    def render_video(self, time):
+    def render_video(self, time: float):
         pygame.surfarray.blit_array(self.surface, self.videoplayer.get_frame(time))
 
     def loop(self, events):
