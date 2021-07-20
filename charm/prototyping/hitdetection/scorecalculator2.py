07c1209754d9857ca0c7043bd6ec9cc18c75bee5
DigiDuncan <digiduncan@gmail.com>
2021-07-19 21:40:12 -0400

    this was all a bad idea (maybe)

--- charm/prototyping/hitdetection/scorecalculator2.py
+++ charm/prototyping/hitdetection/scorecalculator2.py
@@ -1,5 +1,36 @@
-from charm.song import Chart
+from nindex import Index
+from charm.song import Chart, TempoCalculator
 from charm.lib.instruments.guitar import Guitar
+
+
+class TempoTape:
+    def __init__(self, index: Index, tempo_calc: TempoCalculator, scanner_width: float) -> None:
+        self._index = index
+        self._tempo_calc = tempo_calc
+        self.scanner_width = scanner_width
+
+        self.current_position = 0  # seconds
+
+        self.current_items = []
+        self.dirty_ids = []
+
+    @property
+    def timeslice(self):
+        offset = self.scanner_width / 2
+        end = self._tempo_calc.secs_to_ticks(self.current_position + offset)
+        start = self._tempo_calc.secs_to_ticks(self.current_position - offset)
+        start = max(0, start)
+        return slice(start, end, 1)
+
+    def scan(self):
+        self.current_items = self._index[self.timeslice]
+        self.current_items = [i for i in self.current_items if i.id not in self.dirty_ids]
+
+    def set_position(self, position: float):
+        self.current_position = position
+
+    def scroll(self, amount: float):
+        self.current_position += amount
 
 
 class ScoreCalculator:
@@ -7,11 +38,19 @@ class ScoreCalculator:
         self.chart = chart
         self.song = self.chart.song
         self.guitar = guitar
-
         self._hitwindow = 0.140
-        self.hitwindow = self.song.tempo_calc.secs_to_ticks(self._hitwindow)
+
+        self.chord_tape = TempoTape(self.chart.chord_by_ticks, self.song.tempo_calc, self._hitwindow)
 
         self._events = []
+
+        self.score = 0
+        self.streak = 0
+        self.accuracies = []
+        self.multiplier = 1
+
+    def update(self, time):
+        pass
 
 # --- EVENTS ---
 
@@ -21,10 +60,10 @@ class ScoreEvent:
         self.ticks = ticks
 
     def __lt__(self, other):
-        self.ticks < self.ticks
+        self.ticks < other.ticks
 
     def __eq__(self, other):
-        self.ticks == self.ticks
+        self.ticks == other.ticks
 
     def __str__(self) -> str:
         return f"<{self.__class__.__name__}: {self.ticks}>"
